d4b297334ad50d36e0bd2f1544fb17d3be6ae926
jumpjack <jumpjack@libero.it>
2023-02-14 10:05:45 +0100

    Create mylocations.js

---
+++ mylocations.js
@@ -0,0 +1,9 @@
+myLocations = [
+{ lat : 42.060691,  lng : 12.80653, height : 1374, name : "Monte gennaro (vetta)"},
+{ lat : 42.060930,  lng :  12.800319, height : 1274, name : "Monte gennaro (rifugio)"},
+{ lat : 41.902139, lng : 12.453384, height: 213, name : "Cupola San Pietro"},
+{ lat : 42.473236, lng : 12.997127, height: 3661, name : "Terminillo"},
+{ lat : 42.009892, lng : 12.841129, height: 779, name : "San Polo dei Cavalieri"},
+]
+
+ 
