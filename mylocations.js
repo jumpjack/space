e8d918d380f1a63b74138e2e399daed9eca12a4b
jumpjack <jumpjack@libero.it>
2023-12-28 13:18:16 +0100

    Update mylocations.js

--- mylocations.js
+++ mylocations.js
@@ -4,9 +4,9 @@ myLocations = [
 { lat : 41.902139, lng : 12.453384, height: 213, name : "Cupola San Pietro"},
 { lat : 42.473236, lng : 12.997127, height: 3661, name : "Terminillo"},
 { lat : 42.009892, lng : 12.841129, height: 779, name : "San Polo dei Cavalieri"},
-{ lat : 45.080779, lng : 7.767619, height: 680, name : "Basilica di Superga (Torino)"},
-{ lat : 45.668002, lng : 7.090502, height: 3779, name : "Monviso"},
- { lat : 45.139278, lng : 7.865018, height: 350, name : "Foto2023"},
+{ lat : 45.080778, lng : 7.767618, height: 680, name : "Basilica di Superga (Torino)"},
+{ lat : 45.668003, lng : 7.090503, height: 3779, name : "Monviso"},
+{ lat : 45.139278, lng : 7.865018, height: 350, name : "Foto2023"},
 ]
 
  
