ad35b8949b8ab54704a9f06d0177de11c4a01dda
jumpjack <jumpjack@libero.it>
2024-03-19 09:07:27 +0100

    Update mylocations.js

--- mylocations.js
+++ mylocations.js
@@ -1,16 +1,16 @@
 myLocations = [
-{ lat : 42.060691, lng : 12.80653, height : 1374, name : "Monte gennaro (vetta)"},
-{ lat : 42.060930, lng :  12.800319, height : 1274, name : "Monte gennaro (rifugio)"},
-{ lat : 42.035907, lng :  12.711954, height : 1274, name : "Sant'Angelo Romano"},
-{ lat : 41.902139, lng : 12.453384, height: 213, name : "Cupola San Pietro"},
-{ lat : 41.858904, lng : 12.565374, height: 213, name : "Cupola Don Bosco"},
-{ lat : 42.473236, lng : 12.997127, height: 3661, name : "Terminillo"},
-{ lat : 42.009892, lng : 12.841129, height: 779, name : "San Polo dei Cavalieri"},
-{ lat : 45.080680, lng : 7.768051, height: 680, name : "Basilica di Superga (Torino)"},
-{ lat : 44.667312, lng : 7.090381, height: 3779, name : "Monviso"},
-{ lat : 45.127805, lng : 7.846985, height: 350, name : "Foto2023"},
-{ lat : -22.9524648, lng: -43.2105244, height : 73, name : "Cristo redentore"},
-{ lat : -22.922725, lng: -43.095310, height : 0, name : "Foto 4/05/2023"}, 
+ { lat : 42.060691,   lng : 12.80653,   height:  1374,  name : "Monte gennaro (vetta)"},
+ { lat : 42.060930,   lng : 12.800319,  height:  1274,  name : "Monte gennaro (rifugio)"},
+ { lat : 42.035907,   lng : 12.711954,  height:  400,  name : "Sant'Angelo Romano"},
+ { lat : 41.902139,   lng : 12.453384,  height:   213,  name : "Cupola San Pietro"},
+ { lat : 41.858904,   lng : 12.565374,  height:   213,  name : "Cupola Don Bosco"},
+ { lat : 42.473236,   lng : 12.997127,  height:  3661,  name : "Terminillo"},
+ { lat : 42.009892,   lng : 12.841129,  height:   779,  name : "San Polo dei Cavalieri"},
+ { lat : 45.080680,   lng :  7.768051,  height:   680,  name : "Basilica di Superga (Torino)"},
+ { lat : 44.667312,   lng :  7.090381,  height:  3779,  name : "Monviso"},
+ { lat : 45.127805,   lng :  7.846985,  height:   350,  name : "Foto2023"},
+ { lat : -22.9524648, lng: -43.2105244, height:    73,  name : "Cristo redentore"},
+ { lat : -22.922725,  lng: -43.095310,  height:     0,  name : "Foto 4/05/2023"}, 
 ]
 
  
